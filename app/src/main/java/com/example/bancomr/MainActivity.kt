309a38a1ddc20b9a2efe1f0e11447deb3f59d78b
Josebias Neto <josebiasbsneto@gmail.com>
2021-06-13 17:13:32 -0300

    Add Activity Saldo

--- app/src/main/java/com/example/bancomr/MainActivity.kt
+++ app/src/main/java/com/example/bancomr/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.bancomr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bancomr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivSaldo.setOnClickListener{
+            val intent = Intent(this, Saldo::class.java)
+            startActivity(intent)
+        }
     }
 }
